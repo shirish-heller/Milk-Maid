32c8aae0c4f293e5674b8655976baa4e0958058f
Shivansh Rajolia <shivansh.rajolia28@gmail.com>
2018-05-04 10:33:31 +0530

    Setup for font-Awesome path through sever and completing NAv-Bar

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome/'));
 app.listen(3500, (req, res)=> {
     console.log('OUR WEB PAGE WORKS !!!!--from server.js listening to 3500');
 });
