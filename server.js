ae9ce1e05056e4ee34fb0d03c4e34ec07fb13fa9
Shivansh Rajolia <shivansh.rajolia28@gmail.com>
2018-05-04 11:27:37 +0530

    Defining Jquery and bootstrap routes for npm usage

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome/'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 app.listen(3500, (req, res)=> {
     console.log('OUR WEB PAGE WORKS !!!!--from server.js listening to 3500');
 });
