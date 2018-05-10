59fef3ad02dd2d25fd188b4dbc1b467770c60f42
Shivansh Rajolia <shivansh.rajolia28@gmail.com>
2018-05-10 11:24:16 +0530

    Loading Indicator removing unwanted HBS and adding bg gradient

--- js/index.js
+++ js/index.js
@@ -1,32 +1,4 @@
 var masterPanel = require("../handlebars/masterPanel.hbs");
-var loadingIndicator = require("../handlebars/loading-indicator.hbs");
-var busy = {busy: true};
-// $(document).ready(function () {
-//   $.ajax({
-//     xhr: function (){
-//       var xhr = new XMLHttpRequest();
-//       console.log('xhr', JSON.stringify(xhr));
-//       xhr.addEventListener('progress', function(e) {
-//         if(e.lengthComputable) {
-//           console.log('Percent uploaded', e.loaded/e.total *100);
-//         }
-//       });
-//     },
-//     type: "GET",
-//     url: "https://api.myjson.com/bins/lkta2",
-//     processData: false,
-//     contentType: false,
-//     success: function(res) {
-//       console.log(res);
-//       createHTML(res);
-//     },
-//     error: function(err) {
-//       console.log('error', err)
-//     }
-//   });
-// });
-
-
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://api.myjson.com/bins/lkta2');
 ourRequest.onload = function() {
@@ -34,7 +6,6 @@ ourRequest.onload = function() {
     var data = JSON.parse(ourRequest.responseText);
     console.log(data);
     setTimeout(function() {
-      console.log('now');
       createHTML(data);
     },2000);
   } else {
@@ -53,6 +24,5 @@ function createHTML(productData) {
   productCategories.innerHTML = masterPanel(productData);
 
   var loadingIndTemp = document.getElementById("loading-indicator");
-  busy = {busy: false};
-  loadingIndTemp.innerHTML = loadingIndicator(busy);
+  loadingIndTemp.innerHTML = "";
 }
